db7ddef47b3a3b8e868876c7d444c771661c3150
spielchen <anna.bukavtsova@gmail.com>
2019-03-01 14:43:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var num = 33721;
+
+var splitNum = Number( eval(num.toString().split('').join('*')) );
+console.log(splitNum);
+console.log(typeof(splitNum));
+var step = splitNum**3;
+console.log(step);
+
+var stringNum = step.toString()[0]+step.toString()[1];
+console.log("вывести первые две цифры: "+ stringNum);
